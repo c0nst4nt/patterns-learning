0132060b9bdc4a60f1bf9e1785ff0578bae80489
kgordienko <kgordienko@gamepoint.com>
2019-09-04 18:08:37 +0300

    Added code and test for facade pattern.

---
+++ code/src/Structural/Facade/Facade.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DesignPatterns\Structural\Facade;
+
+class Facade
+{
+    /**
+     * @var OperatingSystem
+     */
+    private $os;
+
+    /**
+     * @var Bios
+     */
+    private $bios;
+
+    /**
+     * @param Bios $bios
+     * @param OperatingSystem $os
+     */
+    public function __construct(Bios $bios, OperatingSystem $os)
+    {
+        $this->bios = $bios;
+        $this->os   = $os;
+    }
+
+    public function turnOn()
+    {
+        $this->bios->execute();
+        $this->bios->waitForKeyPress();
+        $this->bios->launch($this->os);
+    }
+
+    public function turnOff()
+    {
+        $this->os->halt();
+        $this->bios->powerDown();
+    }
+}
